077929316bad7364d86dd39d2d220d83688ea6bd
amirfazel <amirfazel45@gmail.com>
2021-11-04 16:45:25 +0330

    first layot design completed

---
+++ src/Layots/MainLayot.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+function MainLayot(prpos){
+    return <React.Fragment >
+   <section className="row vh-100">
+   <div className="col-8 my-auto p-5  ">
+   {prpos.children}
+   </div>
+   <div className="col-4 bg-success d-flex row  justify-content-around">
+   {/* Titles and text section */}
+   <div className="p-2">
+       <h1 className="text-end">تایتل</h1>
+       <h3 className="text-end">ساب تایتل</h3>
+       <p className="text-end">
+           لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد کتابهای زیادی در شصت و سه درصد گذشته حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی            
+       </p>
+   </div>
+   {/* End of titles and text section */}
+    {/* input & buttons section */}
+   <div className="d-flex flex-column  justify-content-between">
+   <input type='text' placeholder="متن ورودی"  className="text-end w-25 h-25 mx-"></input>
+   <input type="button" className="w-25 h-25" value="submit"></input>
+   </div>
+   {/* End of input and buttons */}
+   </div>
+  
+   </section>
+
+  </React.Fragment>;
+}
+
+
+export default MainLayot;
--- src/containers/terminal/Terminal.js
+++ src/containers/terminal/Terminal.js
@@ -1,17 +1,21 @@
 import React, { Component } from "react";
 import SimpleTerminal from "../../components/terminal/SimpleTerminal";
+import MainLayot from "../../Layots/MainLayot";
 
 function Terminal() {
-  return <React.Fragment >
-   <section className="row vh-100">
-   <div className="col-8 my-auto p-5  ">
-   <SimpleTerminal className=""/>
-   </div>
-   <div className="col-4 bg-success">
-   side part
-   </div>
-   </section>
+  // return <React.Fragment >
+  //  <section className="row vh-100">
+  //  <div className="col-8 my-auto p-5  ">
+  //  <SimpleTerminal className=""/>
+  //  </div>
+  //  <div className="col-4 bg-success">
+  //  side part
+  //  </div>
+  //  </section>
 
-  </React.Fragment>;
+  // </React.Fragment>;
+  return <MainLayot>
+    <SimpleTerminal></SimpleTerminal>
+  </MainLayot>
 }
 export default Terminal;
