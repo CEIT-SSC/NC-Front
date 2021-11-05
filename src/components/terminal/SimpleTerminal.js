ea11a26201ec6ab5d52deecfd61be284e3c96480
amirfazel <amirfazel45@gmail.com>
2021-11-05 22:35:19 +0330

    terminal text color changed

--- src/components/terminal/SimpleTerminal.js
+++ src/components/terminal/SimpleTerminal.js
@@ -4,6 +4,9 @@ import useTerminal from "../../hooks/useTerminal";
 
 function SimpleTerminal() {
   const commands = useTerminal();
-  return <Terminal commands={commands} promptLabel={"NOOB1$"}  style={{backgroundColor:`#f6e6d6`, boxShadow: '5px 6px 13px 0px var(--button-shadow'}} promptLabelStyle={{color:'#0a038f',fontSize:'1rem'}}/>;
+  return <Terminal commands={commands} promptLabel={"NOOB1$"}  style={{backgroundColor:`#f6e6d6`, boxShadow: '5px 6px 13px 0px var(--button-shadow'}} 
+  promptLabelStyle={{color:'#0a038f',fontSize:'1rem'}}
+  contentStyle={{color:`#382154`}}
+  />;
 }
 export default SimpleTerminal;
