6eda4ecbdd966f0aa599b8618627d04f991f536e
amirfazel <amirfazel45@gmail.com>
2021-11-06 11:55:50 +0330

    terminal elements spacing fixed

--- src/components/terminal/SimpleTerminal.js
+++ src/components/terminal/SimpleTerminal.js
@@ -5,9 +5,9 @@ import useTerminal from "../../hooks/useTerminal";
 function SimpleTerminal() {
   const commands = useTerminal();
   return <Terminal commands={commands} promptLabel={"NOOB1$"}  style={{backgroundColor:`#f6e6d6`, boxShadow: '5px 6px 13px 0px var(--button-shadow' ,borderRadius:`15px`}} 
-  promptLabelStyle={{color:'#0a038f',fontSize:'1rem',marginTop:`8px`}}
+  promptLabelStyle={{color:'#0a038f',fontSize:'1rem',paddingTop:`16px`}}
   contentStyle={{color:`#382154`}}
-  inputTextStyle={{color:`#0a0c26` ,fontSize:`1rem`,marginTop:`10px`}}
+  inputTextStyle={{color:`#0a0c26` ,fontSize:`1rem`,marginTop:`15.8px`}}
   />;
 }
 export default SimpleTerminal;
