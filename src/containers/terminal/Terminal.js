06fe7ff5d49846d671c9b29fae357f1be1cbe2b1
amirfazel <amirfazel45@gmail.com>
2021-11-06 23:10:24 +0330

    auto scroll added to side layout  description

--- src/containers/terminal/Terminal.js
+++ src/containers/terminal/Terminal.js
@@ -3,17 +3,6 @@ import SimpleTerminal from "../../components/terminal/SimpleTerminal";
 import MainLayot from "../../Layouts/MainLayout";
 
 function Terminal() {
-  // return <React.Fragment >
-  //  <section className="row vh-100">
-  //  <div className="col-8 my-auto p-5  ">
-  //  <SimpleTerminal className=""/>
-  //  </div>
-  //  <div className="col-4 bg-success">
-  //  side part
-  //  </div>
-  //  </section>
-
-  // </React.Fragment>;
   return <MainLayot>
     <SimpleTerminal></SimpleTerminal>
   </MainLayot>
