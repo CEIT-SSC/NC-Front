792bf519e3fcc80aeec807b0ecb077b2fef0a096
farzad80rad <f.radnia2@gmail.com>
2021-11-21 16:39:22 +0330

    debug users api header

--- src/api/user.js
+++ src/api/user.js
@@ -3,26 +3,47 @@ import apiAddresses from "./info";
 
 async function login(userName, password, student_number) {
   console.log("tsssss login api");
-  return await axios.post(apiAddresses + "/user/login", {
-    username: userName,
-    password: password,
-    student_number: student_number,
-  });
+  return await axios.post(
+    apiAddresses + "/user/login",
+    {
+      username: userName,
+      password: password,
+      student_number: student_number,
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
 }
 
 async function register(userName, password, student_number) {
-  return await axios.post(apiAddresses + "/user/register", {
-    username: userName,
-    password: password,
-    student_number: student_number,
-  });
+  return await axios.post(
+    apiAddresses + "/user/register",
+    {
+      username: userName,
+      password: password,
+      student_number: student_number,
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
 }
 
 async function logout(token) {
   return await axios.post(
     apiAddresses + "/user/logout",
     {},
-    { authorization: "Bearer " + token }
+    {
+      headers: {
+        "Content-Type": "application/json",
+        authorization: "Bearer " + token,
+      },
+    }
   );
 }
 
