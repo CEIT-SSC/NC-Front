2138b8503a647daebf00a8cec2e5cb27690e8d56
farzad80rad <f.radnia2@gmail.com>
2021-11-21 15:54:57 +0330

    add axios

---
+++ src/api/user.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+import apiAddresses from "./info";
+
+async function login(userName, password, student_number) {
+  return await axios.post(apiAddresses + "/user/login", {
+    username: userName,
+    password: password,
+    student_number: student_number,
+  });
+}
+
+async function register(userName, password, student_number) {
+  return await axios.post(apiAddresses + "/user/register", {
+    username: userName,
+    password: password,
+    student_number: student_number,
+  });
+}
+
+async function logout(token) {
+  return await axios.post(
+    apiAddresses + "/user/logout",
+    {},
+    { authorization: "Bearer " + token }
+  );
+}
+
+export { register, login, logout };
--- src/store/features/userSlice.js
+++ src/store/features/userSlice.js
@@ -1,4 +1,24 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { login, register, logout } from "../../api/user";
+
+const loginUser = createAsyncThunk(
+  "user/loginUser",
+  async (username, password, student_number) => {
+    const response = await login(username, password, student_number);
+    return response.data;
+  }
+);
+const registerUser = createAsyncThunk(
+  "user/registerUser",
+  async (username, password, student_number) => {
+    const response = await register(username, password, student_number);
+    return response.data;
+  }
+);
+const logoutUser = createAsyncThunk("user/logoutUser", async (token) => {
+  const response = await logout(token);
+  return response.data;
+});
 
 export const userSlice = createSlice({
   name: "user",
@@ -18,14 +38,17 @@ export const userSlice = createSlice({
       state.token = action.payload;
     },
   },
-});
-
-export const login = createAsyncThunk("posts/fetchPosts", async () => {
-  const response = await client.get("/fakeApi/posts");
-  return response.data;
+  extraReducers: {
+    [loginUser.fulfilled]: (state, action) => {
+      state.username = action.payload.username;
+      state.student_number = action.payload.student_number;
+      state.token = action.payload.token;
+    },
+  },
 });
 
 // Action creators are generated for each case reducer function
 export const { setUsername, setStudentNumber, setToken } = userSlice.actions;
+export { loginUser, logoutUser, registerUser };
 
 export default userSlice.reducer;
